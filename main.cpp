6cfdeb218913161977ace4b838426206dd387ee1
Uahh <hnyrarara@gmail.com>
2021-02-09 15:45:23 +0800

    整理代码

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//
+//  main.cpp
+//  flac
+//
+//  Created by Uahh on 2021/2/8.
+//
+
+#include <stdio.h>
---
+++ main.hpp
@@ -0,0 +1,13 @@
+//
+//  main.hpp
+//  flac
+//
+//  Created by Uahh on 2021/2/8.
+//
+
+#ifndef main_hpp
+#define main_hpp
+
+#include <stdio.h>
+
+#endif /* main_hpp */
